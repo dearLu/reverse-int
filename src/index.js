6047cbc85220a206ef69dbb4bec5dc9f3f1bfc29
dearLu <lady.barabanovapolina@list.ru>
2020-10-25 19:40:59 +0400

    feat: add method reverse

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 module.exports = function reverse (n) {
-  
+    let result=n;
+    if(result<0){
+            result=n*(-1);
+    }
+    let wholePath;
+    let decadePart;
+    let hundredPart;
+    wholePath=parseInt(result/100);
+    decadePart=parseInt((result%100)/10);
+    hundredPart=result%10;
+    
+    return Number(`${hundredPart}${decadePart}${wholePath}`);
 }
